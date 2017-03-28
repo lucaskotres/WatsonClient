83c8dd29c9e75a47695d05f74f5d4050415e2903
kotres <lucaskotres@gmail.com>
2017-03-28 17:09:46 -0300

    Chat included

--- FlaskChat/app/main/events.py
+++ FlaskChat/app/main/events.py
@@ -1,6 +1,17 @@
 from flask import session
 from flask_socketio import emit, join_room, leave_room
 from .. import socketio
+import json
+from watson_developer_cloud import ConversationV1
+
+conversation = ConversationV1(
+    username='48668bda-7cdc-4e2c-b98b-61d8498fd93b',
+    password='QzdhAsFJLly6',
+    version='2017-03-28')
+
+# replace with your own workspace_id
+workspace_id = '97f9058e-9f52-4d7c-8bba-bb45d485c071'
+
 
 
 @socketio.on('joined', namespace='/chat')
@@ -10,6 +21,8 @@ def joined(message):
     room = session.get('room')
     join_room(room)
     emit('status', {'msg': session.get('name') + ' iniciou o chat'}, room=room)
+    response = conversation.message(workspace_id=workspace_id, message_input={
+        'text': 'Ola'})
 
 
 @socketio.on('text', namespace='/chat')
@@ -18,6 +31,13 @@ def text(message):
     The message is sent to all people in the room."""
     room = session.get('room')
     emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+
+    response = conversation.message(workspace_id=workspace_id, message_input={
+        'text': message['msg']}, context=response['context'])
+
+    print(json.dumps(response, indent=2))
+    emit('message', {'msg': 'SuporteBot' + ':' + json.dumps(response, indent=2)}, room=room)
+    #emit('message',{'msg': 'SuporteBot' + ':' + json.dumps(response['text'])},room=room)
 
 
 @socketio.on('left', namespace='/chat')
